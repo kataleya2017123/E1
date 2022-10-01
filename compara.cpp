9fad4246cfa05d691e52bfc015a6d4d454b50329
GerarGarces <janeth.altafuya.mideros@utelvt.edu.ec>
2022-10-01 19:09:36 +0000

    programa compara.cpp

---
+++ compara.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  float k,p;
+  cout<<"Ingrese k  : "; cin>>k;
+  cout<<"Ingrese p  : "; cin>>p;
+  if(k==p){
+     cout<<"Son iguales"<<endl;
+  }else{
+     if(k<p){
+       cout<<k<<" es el menor que "<<p<<"\n";
+     }else{
+       cout<<p<<" es el menor que "<<k<<"\n";
+     }
+  }
+
+return (0);;
+
+
+
+
+
+
+}
